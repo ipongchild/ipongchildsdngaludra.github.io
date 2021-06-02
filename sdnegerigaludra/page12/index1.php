070012e9783e5a5773fe2f87ebbb584252bbdeb8
ipongchild <81487481+ipongchild@users.noreply.github.com>
2021-06-03 01:31:13 +0700

    Add files via upload

---
+++ sdnegerigaludra/page12/index1.php
@@ -0,0 +1,201 @@
+<!-- sambungkan ke function-->
+<?php
+require '../function/function.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> <?PHP echo "$titlesekolah1"; ?> </title>
+        <link rel="stylesheet" href="stylesheet.css">
+        <link rel="stylesheet" href="responsive.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        
+
+
+
+       
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.79.0">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/navbars/">
+
+    
+
+    <!-- navbars core CSS -->
+<link href="../page12/navbars/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="navbar.css" rel="stylesheet">
+
+
+
+
+        
+    </head>
+    <body>
+        <header>
+        <div class="container">
+            <div class="header-left">
+            <img  class="logo" src="../startbootstrap-sb-admin-2-gh-pages/img/Logo/logo-tutwuri-handayani.png" alt=""> -->
+            </div> </div> 
+            
+
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="#">SDN GALUDRA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample03">
+                <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                <li class="nav-item active">
+                    <a class="nav-link" aria-current="page" href="#">Beranda</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Berita</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                    <li><a class="dropdown-item" href="#">Agenda Kegiatan</a></li>
+                    <li><a class="dropdown-item" href="#">Libur Sekolah</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Unduhan</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                    <li><a class="dropdown-item" href="#">RPP</a></li>
+                    <li><a class="dropdown-item" href="#">Kalender Pendidikan</a></li>
+                    <li><a class="dropdown-item" href="#">KKO</a></li>
+                    <li><a class="dropdown-item" href="#">Teori Para Ahli</a></li>
+                    <li><a class="dropdown-item" href="#">Alat Peraga</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Galeri</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                    <li><a class="dropdown-item" href="#">Album Foto</a></li>
+                    <li><a class="dropdown-item" href="#">Video</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontak</a>
+                </li>
+                </ul>
+                <form>
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+            </div>
+        </nav>
+
+
+
+
+
+
+
+        </header>
+        <div class="top-wrapper">
+        <div class="container">
+            <h1>SD NEGERI GALUDRA.</h1>
+            <h1>KREATIFITAS TANPA BATAS.</h1>
+            <p>Progate adalah platform online untuk belajar coding.</p>
+            <p>Kami menawarkan lingkungan pemograman yang lengkap untuk mempermudah Anda memulai.</p>
+            <div class="btn-wrapper">
+            <a href="#" class="btn signup">Daftar dengan Email</a>
+            <p>atau</p>
+            <a href="#" class="btn facebook"><span class="fa fa-facebook"></span>Daftar dengan Facebook</a>
+            <a href="#" class="btn twitter"><span class="fa fa-twitter"></span>Daftar dengan Twitter</a>
+            </div>
+        </div>
+        </div>
+        <div class="lesson-wrapper">
+        <div class="container">
+            <div class="heading">
+            <h2>Cari tau dari mana anda mau memulai!</h2>
+            </div>
+            <div class="lessons">
+            <div class="lesson">
+                <div class="lesson-icon">
+                <img src="https://prog-8.com/images/html/advanced/html.png">
+                <p>HTML & CSS</p>
+                </div>
+                <p class="text-contents">Bahasa yang digunakan untuk membuat dan mendesain tampilan situs web Anda. </p>
+            </div>
+            <div class="lesson">
+                <div class="lesson-icon">
+                <img src="https://prog-8.com/images/html/advanced/jQuery.png">
+                <p>jQuery</p>
+                </div>
+                <p class="text-contents">Library JavaScript yang cepat, kaya akan fitur, dan mudah digunakan yang menangani animasi dan permintaan Ajax.</p>
+            </div>
+            <div class="lesson">
+                <div class="lesson-icon">
+                <img src="https://prog-8.com/images/html/advanced/ruby.png">
+                <p>Ruby</p>
+                </div>
+                <p class="text-contents">Bahasa dinamis, serba guna yang sederhana dan produktif. Ruby sering digunakan untuk membuat aplikasi web yang responsive.</p>
+            </div>
+            <div class="lesson">
+                <div class="lesson-icon">
+                <img src="https://prog-8.com/images/html/advanced/php.png">
+                <p>PHP</p>
+                </div>
+                <p class="text-contents">Bahasa skrip open source yang dapat disematkan ke dalam HTML, dan cocok untuk pengembangan web.</p>
+            </div>
+            <div class="clear"></div>
+            </div>
+        </div>
+        </div>
+        <div class="message-wrapper">
+        <div class="container">
+            <div class="heading">
+            <h2>Apakah anda siap untuk menjadi programer yang handal?</h2>
+            <h3>Ayo belajar coding, ayo belajar menjadi lebih kreatif!</h3>
+            </div>
+            <span class="btn message">Mulai Belajar</span>
+        </div>
+        </div>
+        <footer>
+        <div class="container">
+            <img src="https://prog-8.com/images/html/advanced/footer_logo.png">
+            <p>Learn to code, learn to be creative.</p>
+        </div>
+        </footer>
+
+
+
+
+
+        <script src="../page12/navbars/bootstrap.bundle.min.js"></script>
+
+
+
+
+    </body>
+</html>
